435e8711912e540b1de4a131e91efc4974be2a78
Zoran Zaric <zz@zoranzaric.de>
2021-05-16 10:41:31 +0200

    Copy frankenstein example

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use frankenstein::Api;
+use frankenstein::ChatIdEnum;
+use frankenstein::GetUpdatesParams;
+use frankenstein::SendMessageParams;
+use frankenstein::TelegramApi;
+
+fn main() {
+    let token = std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+
+    let api = Api::new(token.to_string());
+
+    let mut update_params = GetUpdatesParams::new();
+    update_params.set_allowed_updates(Some(vec!["message".to_string()]));
+
+    loop {
+        let result = api.get_updates(&update_params);
+
+        println!("result: {:?}", result);
+
+        match result {
+            Ok(response) => {
+                for update in response.result {
+                    if let Some(message) = update.message() {
+                        let mut send_message_params = SendMessageParams::new(
+                            ChatIdEnum::IsizeVariant(message.chat().id()),
+                            "hello".to_string(),
+                        );
+                        send_message_params.set_reply_to_message_id(Some(message.message_id()));
+
+                        let _ = api.send_message(&send_message_params);
+
+                        update_params.set_offset(Some(update.update_id() + 1))
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Failed to get updates: {:?}", error);
+            }
+        }
+    }
+}
